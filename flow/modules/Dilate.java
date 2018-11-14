f81fdeca54fcb9d15074a30da7f0b23d0abdf3de
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2018-11-13 22:12:37 -0500

    left-dilate

--- flow/modules/Dilate.java
+++ flow/modules/Dilate.java
@@ -18,18 +18,22 @@ public class Dilate extends Unit
     private static final long serialVersionUID = 1;
 
     public static final int MOD_CUT = 0;
+
+    public static final int DIRECTION_LEFT = 0;
+    public static final int DIRECTION_RIGHT = 1;
+    public static final int DIRECTION_BOTH = 2;
      
-    boolean right;
-    public boolean getRight() { return right; }
-    public void setRight(boolean val) { right = val; }
+    int direction = DIRECTION_BOTH;
+    public int getDirection() { return direction; }
+    public void setDirection(int val) { direction = val; }
      
-    public static final int OPTION_RIGHT = 0;
+    public static final int OPTION_DIRECTION = 0;
 
     public int getOptionValue(int option) 
         { 
         switch(option)
             {
-            case OPTION_RIGHT: return getRight() ? 1 : 0;
+            case OPTION_DIRECTION: return getDirection();
             default: throw new RuntimeException("No such option " + option);
             }
         }
@@ -38,7 +42,7 @@ public class Dilate extends Unit
         { 
         switch(option)
             {
-            case OPTION_RIGHT: setRight(value != 0); return;
+            case OPTION_DIRECTION: setDirection(value); return;
             default: throw new RuntimeException("No such option " + option);
             }
         }
@@ -49,34 +53,48 @@ public class Dilate extends Unit
         super(sound);
         defineInputs( new Unit[] { Unit.NIL }, new String[] { "Input" });
         defineModulations(new Constant[] { Constant.ZERO }, new String[] { "Boost" });
-        defineOptions(new String[] { "Right" }, new String[][] { { "Right" } });
+        defineOptions(new String[] { "Direction" }, new String[][] { { "Left", "Right", "Both" } });
         }
-                
+	
+	double[] zeros;        
+	double[] upcuts_a;
+	double[] downcuts_a;
+
     public void go()
         {
         super.go();
                 
         pushFrequencies(0);
         copyAmplitudes(0);
-
-        double[] amplitudes = getAmplitudes(0);
         
+        double[] amplitudes = getAmplitudes(0);
+
+		if (zeros == null)
+			{
+			zeros = new double[amplitudes.length];
+			upcuts_a = new double[amplitudes.length];
+			downcuts_a = new double[amplitudes.length];
+			}
+
+		// zero out the arrays
+		System.arraycopy(zeros, 0, upcuts_a, 0, zeros.length);			
+		System.arraycopy(zeros, 0, downcuts_a, 0, zeros.length);			
+
+		double[] upcuts = upcuts_a;
+		double[] downcuts = downcuts_a;
         double cut = 1.0 - modulate(MOD_CUT);
 
-        double[] upcuts = new double[amplitudes.length];
-                
-        upcuts[0] = amplitudes[0];
-        for(int i = 1; i < amplitudes.length; i++)
+        if (direction == DIRECTION_BOTH)
             {
-            if (amplitudes[i] <= upcuts[i - 1])
-                {  upcuts[i] = cut * upcuts[i] + (1 - cut) * upcuts[i - 1]; }
-            else
-                { upcuts[i] = amplitudes[i]; }
-            }
+			upcuts[0] = amplitudes[0];
+			for(int i = 1; i < amplitudes.length; i++)
+				{
+				if (amplitudes[i] <= upcuts[i - 1])
+					{  upcuts[i] = cut * upcuts[i] + (1 - cut) * upcuts[i - 1]; }
+				else
+					{ upcuts[i] = amplitudes[i]; }
+				}
                 
-        if (!right)
-            {
-            double[] downcuts = new double[amplitudes.length];
             downcuts[amplitudes.length - 1] = amplitudes[amplitudes.length - 1];
             for(int i = amplitudes.length - 2; i >= 0; i--)
                 {
@@ -91,13 +109,38 @@ public class Dilate extends Unit
                 amplitudes[i] = Math.max(upcuts[i], downcuts[i]);
                 }
             }
-        else
+        else if (direction == DIRECTION_RIGHT)
             {
+			upcuts[0] = amplitudes[0];
+			for(int i = 1; i < amplitudes.length; i++)
+				{
+				if (amplitudes[i] <= upcuts[i - 1])
+					{  upcuts[i] = cut * upcuts[i] + (1 - cut) * upcuts[i - 1]; }
+				else
+					{ upcuts[i] = amplitudes[i]; }
+				}
+                
             for(int i = 0; i < amplitudes.length; i++)
                 {
                 amplitudes[i] = upcuts[i];
                 }
             }
+        else		// direction == DIRECTION_LEFT
+        	{
+            downcuts[amplitudes.length - 1] = amplitudes[amplitudes.length - 1];
+            for(int i = amplitudes.length - 2; i >= 0; i--)
+                {
+                if (amplitudes[i] <= downcuts[i + 1])
+                    { downcuts[i] = cut * downcuts[i] + (1 - cut) * downcuts[i + 1]; }
+                else
+                    { downcuts[i] = amplitudes[i]; }
+                }
+                                
+            for(int i = 0; i < amplitudes.length; i++)
+                {
+                amplitudes[i] = downcuts[i];
+                }
+        	}
         
         constrain(); 
         }       
