45991c47374b0d2137d4740cbebf6e3a75b9f52e
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2018-12-03 12:41:00 -0500

    Source loading

--- flow/modules/Harmonics.java
+++ flow/modules/Harmonics.java
@@ -5,6 +5,9 @@
 package flow.modules;
 
 import flow.*;
+import java.awt.*;
+import javax.swing.*;
+import flow.gui.*;
 
 /**
    A Unit which allows you to set the amplitudes of
@@ -24,10 +27,11 @@ public class Harmonics extends Unit implements UnitSource
         String[] str = new String[NUM_HARMONICS];
         for(int i = 0; i < NUM_HARMONICS; i++)
             {
-            con[i] = Constant.ZERO;
+            con[i] = new Constant(0);  // we need different Constants so we can update them programmatically, see below
             str[i] = "Amp " + (i + 1);
             }
         defineModulations(con, str);
+        defineInputs( new Unit[] { Unit.NIL }, new String[] { "Source" });
         }
 
     public void go()
@@ -40,4 +44,72 @@ public class Harmonics extends Unit implements UnitSource
             amplitudes[i] = modulate(i);
             }
         }
+
+    public ModulePanel getPanel()
+        {
+        final ModulationInput[] mi = new ModulationInput[NUM_HARMONICS * 2];
+        final ModulePanel[] mp = new ModulePanel[1];
+        mp[0] = new ModulePanel(Harmonics.this)
+            {
+            public JComponent buildPanel()
+                {               
+                Box box = new Box(BoxLayout.Y_AXIS);
+                Unit unit = (Unit) getModulation();
+                
+                box.add(new UnitOutput(unit, 0, this));
+                
+                for(int i = 0; i < NUM_HARMONICS; i++)
+                    {
+                    mi[i] = new ModulationInput(unit, i, this);
+                    box.add(mi[i]);
+                    }
+
+                PushButton sample = new PushButton("Get")
+                	{
+                	public void perform()
+                		{
+	                    Harmonics harmonics = (Harmonics)(getModulation());
+        				int index = harmonics.getSound().findRegistered(harmonics);
+        				Output output = getRack().getOutput();
+        				int numSounds = output.getNumSounds();
+						output.lock();
+						try
+							{
+							double[] amplitudesIn = harmonics.getAmplitudesIn(0);        
+							for(int i = 0; i < NUM_HARMONICS; i++)
+								{
+								double a = amplitudesIn[i];
+								if (a < 0) a = 0;
+								if (a > 1) a = 1;
+								
+								// distribute modulation to all the sounds
+								for(int j = 0; j < numSounds; j++)
+									{
+									Harmonics d = (Harmonics)(output.getSound(j).getRegistered(index));
+
+									if (d.getModulation(i) instanceof Constant)
+										((Constant)(d.getModulation(i))).setValue(a);
+									}
+								}
+							}
+						finally 
+							{
+							output.unlock();
+							}
+						for(int i = 0; i < NUM_HARMONICS; i++)
+							{
+							mi[i].updateText();
+							}
+						mp[0].repaint();
+                		}
+                	};
+                
+                box.add(sample);
+                box.add(new UnitInput(unit, 0, this));
+                return box;
+                }
+            };
+        return mp[0];
+        }
+
     }
--- flow/modules/Partials.java
+++ flow/modules/Partials.java
@@ -20,6 +20,8 @@ public class Partials extends Unit implements UnitSource
     private static final long serialVersionUID = 1;
         
     public static final int NUM_PARTIALS = 16;
+    
+    public static final double FREQUENCY_RANGE = (NUM_PARTIALS * 2);
 
     public Partials(Sound sound) 
         {
@@ -28,14 +30,15 @@ public class Partials extends Unit implements UnitSource
         String[] str = new String[NUM_PARTIALS * 2];
         for(int i = 0; i < NUM_PARTIALS; i++)
             {
-            con[i] = new Constant((i + 1) / (NUM_PARTIALS - 1));
-            con[i + NUM_PARTIALS] = Constant.ZERO;
+            con[i] = new Constant((i + 1) / FREQUENCY_RANGE);
+            con[i + NUM_PARTIALS] = new Constant(0);  // we do this rather than Constant.ZERO so we can change them programmatically via Capture later
             str[i] = "Freq " + (i + 1);
             str[i + NUM_PARTIALS] = "Amp " + (i + 1);
             }
         defineModulations(con, str);
+        defineInputs( new Unit[] { Unit.NIL }, new String[] { "Source" });
         }
-
+        
     public void go()
         {
         super.go();
@@ -45,7 +48,7 @@ public class Partials extends Unit implements UnitSource
               
         for(int i = 0; i < NUM_PARTIALS; i++)
             {
-            frequencies[i] = modulate(i) * (NUM_PARTIALS - 1);
+            frequencies[i] = modulate(i) * FREQUENCY_RANGE;
             amplitudes[i] = modulate(i + NUM_PARTIALS);
             }
         }
@@ -53,17 +56,19 @@ public class Partials extends Unit implements UnitSource
     public String getModulationValueDescription(int modulation, double value, boolean isConstant)
         {
         if (modulation < NUM_PARTIALS && isConstant)
-            return String.format("%.2f", value * (NUM_PARTIALS - 1));
+            return String.format("%.2f", value * FREQUENCY_RANGE);
         else return super.getModulationValueDescription(modulation, value, isConstant);
         }
 
     public ModulePanel getPanel()
         {
-        return new ModulePanel(Partials.this)
+        final ModulationInput[] mi = new ModulationInput[NUM_PARTIALS * 2];
+        final ModulePanel[] mp = new ModulePanel[1];
+        mp[0] = new ModulePanel(Partials.this)
             {
             public JComponent buildPanel()
                 {               
-                JLabel example = new JLabel("" + (NUM_PARTIALS - 1) + ".00");
+                JLabel example = new JLabel("" + FREQUENCY_RANGE + ".00");
                 Box box = new Box(BoxLayout.Y_AXIS);
                 Unit unit = (Unit) getModulation();
                 box.add(new UnitOutput(unit, 0, this));
@@ -72,23 +77,77 @@ public class Partials extends Unit implements UnitSource
                 Box box3 = new Box(BoxLayout.Y_AXIS);
                 for(int i = 0; i < NUM_PARTIALS; i++)
                     {
-                    ModulationInput m = new ModulationInput(unit, i, this);
-                    m.getData().setMinimumSize(example.getMinimumSize());
-                    box3.add(m);
+                    mi[i] = new ModulationInput(unit, i, this);
+                    mi[i].getData().setMinimumSize(example.getMinimumSize());
+                    box3.add(mi[i]);
                     }
                 box2.add(box3);
 
                 Box box4 = new Box(BoxLayout.Y_AXIS);
                 for(int i = NUM_PARTIALS; i < NUM_PARTIALS * 2; i++)
                     {
-                    box4.add(new ModulationInput(unit, i, this));
+                    box4.add(mi[i] = new ModulationInput(unit, i, this));
                     }
                 box2.add(box4);
-                                
                 box.add(box2);
+                
+                PushButton sample = new PushButton("Get")
+                	{
+                	public void perform()
+                		{
+	                    Partials partials = (Partials)(getModulation());
+        				int index = partials.getSound().findRegistered(partials);
+        				Output output = getRack().getOutput();
+        				int numSounds = output.getNumSounds();
+						output.lock();
+						try
+							{
+							double[] amplitudesIn = partials.getAmplitudesIn(0);        
+							double[] frequenciesIn = partials.getFrequenciesIn(0);
+							for(int i = 0; i < NUM_PARTIALS; i++)
+								{
+								// compute the frequency and amplitude
+								double m = frequenciesIn[i] / FREQUENCY_RANGE;
+								if (m < 0) m = 0;
+								if (m > 1) m = 1;
+								
+								double a = amplitudesIn[i];
+								if (a < 0) a = 0;
+								if (a > 1) a = 1;
+								
+								// distribute modulation to all the sounds
+								for(int j = 0; j < numSounds; j++)
+									{
+									Partials d = (Partials)(output.getSound(j).getRegistered(index));
+
+									if (d.getModulation(i) instanceof Constant)
+										((Constant)(d.getModulation(i))).setValue(m);
+									if (d.getModulation(i + NUM_PARTIALS) instanceof Constant)
+										((Constant)(d.getModulation(i + NUM_PARTIALS))).setValue(a);
+									}
+								}
+							}
+						finally 
+							{
+							output.unlock();
+							}
+						for(int i = 0; i < NUM_PARTIALS * 2; i++)
+							{
+							mi[i].updateText();
+							}
+						mp[0].repaint();
+                		}
+                	};
+                
+                Box box5 = new Box(BoxLayout.X_AXIS);
+                box5.add(new UnitInput(unit, 0, this));
+                box5.add(sample);
+                box5.add(Box.createGlue());
+                box.add(box5);
                 return box;
                 }
             };
+        return mp[0];
         }
 
     }
